e4cb2953b2d116d15f36490b9f4acb713e467a23
EriveltonSJ <erivelton.santosj05@gmail.com>
2023-08-28 17:01:44 -0300

    Finalizado

---
+++ script.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector(".no");
+let position;
+
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  btn.addEventListener("touchstart", function () {
+    position ? (position = 0) : (position = 90);
+    btn.style.transform = `translate(${position}px,0px)`;
+    btn.style.transition = "all 0.3s ease";
+  });
+
+  btn.addEventListener("touchend", function () {
+    position ? (position = 0) : (position = 90);
+    btn.style.transform = `translate(${position}px,0px)`;
+    btn.style.transition = "all 0.3s ease";
+  });
+} else {
+  btn.addEventListener("mouseover", function () {
+    position ? (position = 0) : (position = 90);
+    btn.style.transform = `translate(${position}px,0px)`;
+    btn.style.transition = "all 0.3s ease";
+  });
+}
+
+function tanks() {
+  alert("Eu também te amo e aceito namorar com vc! :)");
+}
+
+function not() {
+  alert("Você nem ta maluca de não aceitar, volte lá e clique no sim! :/");
+}
